c62eb301204fe003b8dfe1b040049f3c7e2d89f6
thepoojashetty <03poojashetty@gmail.com>
2017-07-21 15:51:32 +0530

    updated staff

--- Admin/view/php/insert_staff.php
+++ Admin/view/php/insert_staff.php
@@ -8,7 +8,7 @@
     $mob = mysqli_real_escape_string($connect, $data->mob);
     $email = mysqli_real_escape_string($connect, $data->email);
     $jod = mysqli_real_escape_string($connect, $data->jod);
-    $query="INSERT INTO staff(st_name,staff_id,st_mob,st_email,st_join_date) VALUES('$name','$sid','$mob','$email','$jod')";  
+    $query="INSERT INTO staff(st_name,staff_id,st_mob,st_email,st_join_date,pwd) VALUES('$name','$sid','$mob','$email','$jod','$sid')";  
     if(mysqli_query($connect, $query))  
     {  
         echo "Data Inserted...";  
